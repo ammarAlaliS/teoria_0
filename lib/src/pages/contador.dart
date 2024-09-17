d56355b81f5c4e8c5d5930ce0779a8b7d07c73c2
ammarAlaliS <ammaralialiwa@gmail.com>
2024-09-16 18:24:32 -0600

    mostrar imagenes de forma local, de la web y svg

--- lib/src/pages/contador.dart
+++ lib/src/pages/contador.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart'; // Importa el paquete para SVG
 
 class Contador extends StatefulWidget {
   const Contador({super.key});
@@ -8,8 +9,6 @@ class Contador extends StatefulWidget {
 }
 
 class _ContadorState extends State<Contador> {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,37 +21,39 @@ class _ContadorState extends State<Contador> {
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
               height: 250.0,
-              width: 350.0,
-              child: 
-              Image.asset("assets/quickcar.jpg",
+              width: 500,
+              child: Image.asset(
+                "assets/quickcar.jpg",
                 fit: BoxFit.fitWidth,
-              )
-
-              ,
+              ),
             ),
-             SizedBox(
+            SizedBox(
               height: 250.0,
-              width: 350.0,
-              child: 
-              Image.network("https://api.reliasoftware.com/uploads/what_is_flutter_fcb6c7a4b8.png",
+              width: 500,
+              child: Image.network(
+                "https://api.reliasoftware.com/uploads/what_is_flutter_fcb6c7a4b8.png",
                 fit: BoxFit.fitWidth,
-              )
-
-              ,
+              ),
             ),
-
-
+            SizedBox(
+              height: 250.0,
+              width: 500,
+              child: SvgPicture.asset(
+                "assets/t.svg",
+                fit: BoxFit.fitWidth,
+              ),
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () {},
         child: const Icon(Icons.add),
       ),
     );
   }
-
 }
